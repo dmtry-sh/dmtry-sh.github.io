ceb3c36f59c26ab56fb6c3fc933547c90db0f9f3
Dmitry <shuklinov.d@yandex.ru>
2019-10-09 20:35:55 +0300

    Свёрстана страница text + клики в product

--- R-CALL/js/script.js
+++ R-CALL/js/script.js
@@ -1,8 +1,8 @@
 
 
 // =============================== Main slider ===============================
-var next_arrow = document.querySelector('.right_arrow');
-var prev_arrow = document.querySelector('.left_arrow');
+var next_arrow = document.querySelector('.main_title__slider > .right_arrow');
+var prev_arrow = document.querySelector('.main_title__slider > .left_arrow');
 var slides = document.querySelectorAll('.main_title__slider .slide');
 var slider = [];
 
@@ -91,7 +91,7 @@ prev_arrow.onclick = prev;
 
 timer = setInterval(next, 7000);
 
-// ===============================  ===============================
+// =============================== Tabs Slider ===============================
 
 let tabs_items = document.querySelectorAll('.tabs-item');
 let cards = document.querySelectorAll('.card');
@@ -117,16 +117,79 @@ for (let benefit of benefits) {
 
 
 
-// ===============================  ===============================
+// =============================== Hide prev benefits item ===============================
 
 var hover = document.querySelectorAll('.hover');
 var hidden = document.querySelectorAll('.hidden');
 
 function hide(hover, hidden) {
-  hover.onmouseover = (() => hidden.style.visibility = "hidden");
-  hover.onmouseout = (() => hidden.style.visibility = "visible");
+  hover.onmouseover = (() => hidden.style.visibility = 'hidden');
+  hover.onmouseout = (() => hidden.style.visibility = 'visible');
 };
 
 for (i = 0; i < hover.length; i++){
   hide(hover[i], hidden[i]);
 };
+//================================== Product color__list__item =======================
+
+var color_list_items = document.querySelectorAll('.product__color > .color__list > .color__list__item');
+for (let item of color_list_items){
+  item.onclick = function () {
+    item.classList.add('color__list__item--active');
+    for (let other_item of color_list_items) {
+      if (other_item.classList.contains('color__list__item--active')&&
+      other_item.className != item.className) {
+        other_item.classList.remove('color__list__item--active');
+      }
+    }
+  }
+}
+
+//================================== product__list__item  active =======================
+
+var main_product_photo = document.querySelector('.product__photo > .photo');
+var product_photo_items = document.querySelectorAll('.product__list > .product__list__item');
+for (let item of product_photo_items) {
+  item.onclick = function() {
+    item.classList.add('product__list__item--active');
+    main_product_photo.src = item.querySelector('img').src;
+    for (let other_item of product_photo_items) {
+      if (other_item.querySelector('img').src != item.querySelector('img').src) {
+        other_item.classList.remove('product__list__item--active');
+      }
+    }
+  }
+}
+
+// =============================== Open/Close modal  ===============================
+
+var modal_bg = document.querySelector('.modal_background');
+var modal_request_call = document.querySelector('#openRequestCall');
+var modal_add_product = document.querySelector('#openAddProduct');
+var order_consultation = document.querySelector('#openOrderConsultation');
+
+var btn_open_request_call = document.querySelector('.phone .call');
+var btn_close_request_call = document.querySelector('#openRequestCall > .modal_header > .close');
+
+var btn_open_add_product = document.querySelector('.add.button');
+var btn_close_add_product = document.querySelector('#openAddProduct > .modal_header > .close');
+
+function open(modal) {
+  modal_bg.style.opacity = '0.878';
+  modal_bg.style.visibility = 'visible';
+
+  modal.style.opacity = '1';
+  modal.style.visibility = 'visible';
+  modal.style.pointerEvents = 'auto';
+}
+
+function close(modal) {
+  modal_bg.style = null;
+  modal.style = null;
+}
+
+btn_open_request_call.onclick = (() => open(modal_request_call));
+btn_close_request_call.onclick = (() => close(modal_request_call));
+
+btn_open_add_product.onclick = (() => open(modal_add_product));
+btn_close_add_product.onclick = (() => close(modal_add_product));
