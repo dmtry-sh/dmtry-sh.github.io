457c29bcc236dd801783927e5909d9635dc9e8eb
dmtry_sh <shuklinov.d@yandex.ru>
2020-02-23 17:19:50 +0300

    header modified and footer added

--- difiks/gulpfile.js
+++ difiks/gulpfile.js
@@ -1,12 +1,34 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
+var browserSync = require('browser-sync').create()
 
-gulp.task('sass', function() {
-  return gulp.src('app/sass/*.sass')
-    .pipe(sass())
-    .pipe(gulp.dest('app/css'))
-})
+gulp.task('sass', function(done) {
 
-gulp.task('watch', function(){
-  gulp.watch('app/sass/*.sass', gulp.series('sass')); 
-})
+    gulp.src('app/sass/*.sass')
+      .pipe(sass())
+      .pipe(gulp.dest('app/css'))
+      .pipe(browserSync.stream())
+
+    done();
+  
+});
+
+gulp.task('serve', function(done) {
+    
+    browserSync.init({
+        server: "./app"
+    });
+
+    gulp.watch("app/sass/*.sass", gulp.series('sass'));
+    gulp.watch("app/*.html").on('change', () => {
+        browserSync.reload();
+        done();
+    });
+
+    done();
+
+});
+
+gulp.task('default', gulp.series('sass', 'serve'));
+
+
