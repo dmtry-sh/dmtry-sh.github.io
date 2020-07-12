d5339d83603d3c1404ab4714d386f46cb94340d6
dmtry_sh <shuklinov.d@yandex.ru>
2020-07-12 11:21:05 +0300

    768 - 1024 px width done

--- difiks/app/js/script.js
+++ difiks/app/js/script.js
@@ -1,26 +1,26 @@
 'use strict';
 
 function nextSlide() {
-    if (currSlide < 3) {
-        currSlide++;
-        container.style.left = - currSlide * offset + 'px';
-        prev.style.borderColor = 'transparent #000 transparent transparent';
-    }
-    if (currSlide == 3) {
-        next.style.borderColor = 'transparent transparent transparent #8798ab';
-    }
+  if (currSlide < 3) {
+    currSlide++;
+    container.style.left = -currSlide * offset + 'px';
+    prev.style.borderColor = 'transparent #000 transparent transparent';
+  }
+  if (currSlide == 3) {
+    next.style.borderColor = 'transparent transparent transparent #8798ab';
+  }
 }
 
 function prevSlide() {
-    if (currSlide > 0) {
-        currSlide--;
-        container.style.left = - currSlide * offset + 'px';
-        prev.style.borderColor = 'transparent #000 transparent transparent';
-        next.style.borderColor = 'transparent transparent transparent #000';
-    }
-    if (currSlide == 0) {
-        prev.style.borderColor = 'transparent #8798ab transparent transparent';
-    }
+  if (currSlide > 0) {
+    currSlide--;
+    container.style.left = -currSlide * offset + 'px';
+    prev.style.borderColor = 'transparent #000 transparent transparent';
+    next.style.borderColor = 'transparent transparent transparent #000';
+  }
+  if (currSlide == 0) {
+    prev.style.borderColor = 'transparent #8798ab transparent transparent';
+  }
 }
 
 let currSlide = 0;
@@ -30,36 +30,36 @@ const prev = document.querySelector('.prev');
 let offset = container.parentNode.offsetWidth;
 
 prev.addEventListener('click', () => {
-    prevSlide();
+  prevSlide();
 });
 
 next.addEventListener('click', () => {
-    nextSlide();
+  nextSlide();
 });
 
 let scrollListener = new WheelIndicator({
-    elem: document,
-    callback: (e) => {
-        if (e.direction === 'down') {
-            nextSlide();
-        }
-        if (e.direction === 'up') {
-            prevSlide();
-        }
+  elem: document,
+  callback: (e) => {
+    if (e.direction === 'down') {
+      nextSlide();
     }
+    if (e.direction === 'up') {
+      prevSlide();
+    }
+  }
 })
 
 let scrollIsListening = true;
-if (document.documentElement.clientWidth < 960) {
-    scrollListener.turnOff();
-    scrollIsListening = false;
+if (document.documentElement.clientWidth < 1025) {
+  scrollListener.turnOff();
+  scrollIsListening = false;
 }
 
 window.addEventListener('resize', () => {
-    if (scrollIsListening && document.documentElement.clientWidth < 960) {
-        scrollListener.turnOff();
-    }
-    if (!scrollIsListening && document.documentElement.clientWidth >= 960) {
-        scrollListener.turnOn();
-    }
+  if (scrollIsListening && document.documentElement.clientWidth < 1025) {
+    scrollListener.turnOff();
+  }
+  if (!scrollIsListening && document.documentElement.clientWidth >= 1025) {
+    scrollListener.turnOn();
+  }
 })
